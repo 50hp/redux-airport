8e936073bfabdca607c40c6f7b1da5d7b6fd9dab
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-06 11:25:28 -0500

    created the redux reducer, action and store for airles and there planes. added an input for the form to account for planes. created 2 states to track the values of the form inputs. created a function to hadle the submitting of the form.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-
-/** TODO: import REDUX **/
-
+import {createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 /** TODO: Add REDUCERS */
+const airLines  =(state = [], action) => {
+    if (action.type ==="ADD") {
+        return state.concat(action.data);
 
+    }
+    
+            
+
+    return state;
+
+
+};
 
 /** TODO: Create store */
-
+const storeInstance = createStore(
+        combineReducers(
+            {airLines}
+        ),
+        applyMiddleware(
+            logger
+        )
+);
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+      <Provider store={storeInstance}>
         <App />
+      </Provider>
     </React.StrictMode>
 );
